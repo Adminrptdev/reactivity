8296114b4c4348986b67c25c2e40d94607edc8dc
rahul <you@example.com>
2021-08-15 21:45:20 +0530

    Integrated with Client side application

---
+++ client-app/src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1>Reactivities' </h1>
+
+        <ul>
+        {activities.map((activity: any) => (
+            <li key={activity.id}>
+              {activity.title}
+            </li>
+          ))}
+        </ul>
+    </div>
+  );
+}
+
+export default App
